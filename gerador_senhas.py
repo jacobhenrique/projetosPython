9e224f1e6c75246ed4e8efd3dd6871e6fb64db0e
Henrique Jacob <87677730+jacobhenrique@users.noreply.github.com>
2021-11-21 21:44:52 -0300

    gerador_senhas.py
    Gerador de senhas em Python - Projeto - Primeira versão já com interface gráfica.

---
+++ gerador_senhas.py
@@ -0,0 +1,44 @@
+import random
+import PySimpleGUI as sg
+import os
+
+class PassGen:
+    def __init__(self):
+        # Layout
+        sg.theme('Black')
+        layout = [
+            [sg.Text('Site/Software', size=(10,1)),
+             sg.Input(key='site',size=(20,1))],
+            [sg.Text('E-mail/Usuário',size=(10,1)),
+             sg.Input(key='usuario',size=(20,1))],
+            [sg.Text('Quantidade de Caracteres'), sg.Combo(values=list(range(30)),key='total_chars',default_value=1, size=(3,1))],
+            [sg.Output(size=(32,5))],
+            [sg.Button('Gerar Senha')]
+        ]
+        # Declarar a janela
+        self.janela = sg.Window('Password Generator', layout)
+    def Iniciar(self):
+        while True:
+            evento, valores = self.janela.read()
+            if evento == sg.WINDOW.CLOSED:
+                break
+            if evento == 'Gerar Senha':
+                nova_senha = self.gerar_senha(valores)
+                print(nova_senha)
+                self.salvar_senha(nova_senha)
+
+    def gerar_senha(self, valores):
+        char_list = 'ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwyxz1234567890!@#$%¨&*'
+        chars = random.choices(char_list,k=int(valores['total_chars']))
+        new_pass = ''.join(chars)
+        return new_pass
+
+    def salvar_senha(self, nova_senha, valores):
+        with open('senhas.txt','a',newLine='') as arquivo:
+            arquivo.write(f"site: {valores['site']}, usuario: {valores['usuario']}, nova senha: {nova_senha}")
+            
+        print('Arquivo Salvo')
+        
+
+gen = PassGen()
+gen.Iniciar()
