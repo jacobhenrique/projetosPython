dbd0ecfeecde68f90742b374d9d928ee95458842
Henrique Jacob <87677730+jacobhenrique@users.noreply.github.com>
2021-11-30 15:47:01 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -4,5 +4,27 @@ def forca():
     print("Bem Vindo ao Jogo de Forca")
     print("*******------********")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    #enquanto não false e não false
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra?")
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra,index))
+            index = index + 1
+
+
+
+        print("Jogando...")
+
+
+    print("FIM DE JOGO")
+
 if(__name__ == "__main__"):
     forca()
