73a104a015a5ca47b80bba266f9650b62ca8ddc0
Henrique Jacob <87677730+jacobhenrique@users.noreply.github.com>
2021-12-02 15:56:36 -0300

    Alterações no jogo
    Implementado controle de tentativas no jogo da forca.

--- forca.py
+++ forca.py
@@ -4,25 +4,42 @@ def forca():
     print("Bem Vindo ao Jogo de Forca")
     print("*******------********")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     #enquanto não false e não false
     while(not enforcou and not acertou):
 
         chute = input("Qual a letra?")
-        index = 0
-        for letra in palavra_secreta:
-            if(chute == letra):
-                print("Encontrei a letra {} na posição {}".format(letra,index))
-            index = index + 1
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                   letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
 
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
 
-        print("Jogando...")
+        print(letras_acertadas)
 
+    if(acertou):
+        print("Você ganhou!!! :)")
+    else:
+        print("Você perdeu! :(")
 
     print("FIM DE JOGO")
 
